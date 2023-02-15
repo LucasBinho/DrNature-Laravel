4366333a3ba0c9b64d2d4ab69b3c7e73f72a11d4
Lucas_Passos <lucas_passos@msn.com>
2023-02-15 17:24:36 -0300

    Adicao de required aos inputs do formulario para previnir erro de submit com campos vazios

--- DrNature/resources/views/welcome.blade.php
+++ DrNature/resources/views/welcome.blade.php
@@ -9,19 +9,19 @@
     @csrf
       <div class="mb-3">
         <label for="inputCodigo" class="form-label">Código do Produto:</label>
-        <input type="text" name="código" class="form-control" id="inputCodigo">
+        <input type="text" name="código" class="form-control" id="inputCodigo" required>
       </div>
       <div class="mb-3">
           <label for="inputNome" class="form-label">Nome do Produto:</label>
-          <input type="text" name="nome" class="form-control" id="inputNome">
+          <input type="text" name="nome" class="form-control" id="inputNome" required>
         </div>
         <div class="mb-3">
           <label for="inputDescricao" class="form-label">Descrição:</label>
-          <textarea name="descrição" class="form-control" id="inputDescricao" rows="3"></textarea>
+          <textarea name="descrição" class="form-control" id="inputDescricao" rows="3" required></textarea>
         </div>
         <div class="mb-3">
           <label for="inputPreco" class="form-label">Preço:</label>
-          <input type="number" name="preço" class="form-control" id="inputPreco">
+          <input type="number" name="preço" class="form-control" id="inputPreco" required>
         </div>
       <input type="submit" class="btn btn-primary" value="Criar Produto">
       <input type="button" class="btn btn-primary" onclick="window.location='{{ 'produtos' }}'" value="Ver Produtos">
