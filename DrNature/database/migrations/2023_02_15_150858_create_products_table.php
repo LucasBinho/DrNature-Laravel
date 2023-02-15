682af4c57d8b74dd6ddbce5aa5e7ecab3afc8d63
Lucas_Passos <lucas_passos@msn.com>
2023-02-15 12:13:02 -0300

    Criacao e conexão ao banco de dados mysql atraves do XAMPP e criação de tabela de produtos

---
+++ DrNature/database/migrations/2023_02_15_150858_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('código', 100);
+            $table->string('nome', 50);
+            $table->text('descrição');
+            $table->integer('preço');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
